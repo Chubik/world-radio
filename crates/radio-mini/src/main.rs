70e5ca5657abdd723848c766bf2f9c647e7e50b2
Valentyn Chub <vchub@protonmail.com>
2026-06-27 20:43:45 +0300

    chore: scaffold radio-mini as a tauri app

--- crates/radio-mini/src/main.rs
+++ crates/radio-mini/src/main.rs
@@ -1,38 +1,10 @@
-mod app;
+#[allow(dead_code)]
 mod catalog_src;
+#[allow(dead_code)]
 mod state;
-mod theme;
-mod tray;
 
-use eframe::egui;
-
-fn main() -> eframe::Result<()> {
-    radio_core::single_instance::take_over();
-
-    let mut options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([300.0, 200.0])
-            .with_decorations(false)
-            .with_taskbar(false)
-            .with_always_on_top(),
-        ..Default::default()
-    };
-
-    #[cfg(target_os = "macos")]
-    {
-        options.event_loop_builder = Some(Box::new(|builder| {
-            use winit::platform::macos::{ActivationPolicy, EventLoopBuilderExtMacOS};
-            builder.with_activation_policy(ActivationPolicy::Accessory);
-        }));
-    }
-
-    eframe::run_native(
-        "World Radio Mini",
-        options,
-        Box::new(|_cc| {
-            let app = app::MiniApp::new()
-                .map_err(|e| Box::<dyn std::error::Error + Send + Sync>::from(e.to_string()))?;
-            Ok(Box::new(app))
-        }),
-    )
+fn main() {
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
