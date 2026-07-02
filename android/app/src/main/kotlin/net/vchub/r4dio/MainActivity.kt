edb0bf4abf9465642e8b6ac1c2d8a655e0815c10
Valentyn Chub <vchub@protonmail.com>
2026-07-02 17:16:42 +0300

    chore: scaffold android kotlin app project

---
+++ android/app/src/main/kotlin/net/vchub/r4dio/MainActivity.kt
@@ -0,0 +1,15 @@
+package net.vchub.r4dio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("World Radio")
+        }
+    }
+}
