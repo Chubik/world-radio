5d0939f1811321dc7331a24291afcc59755a2f1e
Valentyn Chub <vchub@protonmail.com>
2026-07-04 16:48:56 +0300

    refactor: headless app, control only via tray and home widget

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
 }
 
@@ -27,18 +26,11 @@ android {
         }
     }
 
-    buildFeatures {
-        compose = true
-    }
-
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation(platform("androidx.compose:compose-bom:2025.06.01"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity:1.10.1")
     implementation("androidx.media3:media3-exoplayer:1.6.1")
     implementation("androidx.media3:media3-exoplayer-hls:1.6.1")
     implementation("androidx.media3:media3-session:1.6.1")
