f87b292b013a76907df955de531cdca197399326
ci <ci@users.noreply.github.com>
2026-09-13 09:13:46 +0000

    release: v1.22.12

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "net.vchub.r4dio"
         minSdk = 26
         targetSdk = 37
-        versionCode = 9
-        versionName = "1.4.5"
+        versionCode = 12212
+        versionName = "1.22.12"
     }
 
     compileOptions {
